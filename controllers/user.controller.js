65e78a1e2318c4a0a19c190736e3df74206fd9fb
Toan Nguyen <toannguyen050699@gmail.com>
2019-07-02 10:34:05 +0700

    first commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,54 @@
+var db = require('../db');
+var shortid = require('shortid');
+
+module.exports.index = function(req, res) {
+	res.render('users/index', {
+		users: db.get('users').value()
+	});
+};
+
+module.exports.search = function(req, res){
+	var q = req.query.q;
+	var matchedUsers = db.get('users').value().filter(function(user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/search', {
+		users: matchedUsers
+	});
+};
+
+module.exports.create = function(req, res) {
+	res.render('users/create');
+};
+
+module.exports.get = function(req, res) {
+	var id = req.params.id;
+
+	var user = db.get('users').find({ id: id}).value();
+	res.render('users/view', {
+		user: user
+	});
+};
+
+module.exports.postCreate = function(req,res) {
+	req.body.id = shortid.generate();
+	var errors = [];
+
+	if(!req.body.name) {
+		errors.push('name is required');
+	}
+
+	if(!req.body.phone) {
+		errors.push('phone is required');
+	}	
+
+	if(errors.length) {
+		res.render('users/create', {
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
+	db.get('users').push(req.body).write();
+	res.redirect('/users');
+};
---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var userRoute = require('./routes/user.route');
+
+var port = 2500;
+
+var app =express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		name: 'Toan'
+	});
+});
+
+app.use('/users', userRoute);
+
+
+app.listen(port, function() {
+	console.log('Server listening on port' + port);
+})
